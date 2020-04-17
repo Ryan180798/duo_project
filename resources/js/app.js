b80bcaeed5c5fa9945740f0fc6c71dc224c0a207
Ryan180798 <rjoiret180798@gmail.com>
2020-04-17 09:36:55 +0200

    integration-template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+global.$ = global.jQuery = require('jquery');
+global.Morphext = require('morphext/dist/morphext');
+global.WOW = require('wow.js')
+
+
+require('./jquery.min');
+require('bootstrap');
+require('./hoverIntent');
+require('./superfish');
+require('./sticky');
+require('jquery.easing');
+require('magnific-popup');
+
+require('./custom');
+
+require('./contactform');
---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.app')
+
+@section('content')
+
+    @include('templates.hero')
+
+    @include('templates.header')
+
+    @include('templates.about')
+
+    @include('templates.service')
+
+    @include('templates.subscrbe')
+
+    @include('templates.portfolio')
+
+    @include('templates.testimonials')
+
+    @include('templates.team')
+
+    @include('templates.contact')
+
+    @include('templates.footer')
+    
+@endsection
